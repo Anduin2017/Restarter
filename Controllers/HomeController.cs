20026b90045c8667e05f3ed85c98a4f521445159
Anduin2017 <Anduin@aiursoft.com>
2018-07-05 15:21:51 +0800

    Runnable.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,16 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Restarter.Models;
+using Restarter.Services;
 
 namespace Restarter.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly RestartTrigger _restartTrigger;
+        public HomeController(RestartTrigger restartTrigger)
+        {
+            _restartTrigger = restartTrigger;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        public async Task<IActionResult> Test()
+        {
+            var csServer = new Server
+            {
+                Name = "cs01"
+            };
+            var result = await _restartTrigger.Restart(csServer);
+            return Json(new { message = result });
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
